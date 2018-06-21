d3ecf871c182d76cd692358a76156565f5d2dfdc
dstocker <chos3nmag3@gmail.com>
2018-06-21 16:15:26 -0400

    Changed commenting format

--- src/api_call_managment.py
+++ src/api_call_managment.py
@@ -1,17 +1,22 @@
 import time
 
-import os
 from api_call import *
 
 key = os.environ['API_TOKEN_REGULATIONS_GOV']
 base_url = 'https://api.data.gov:443/regulations/v3/documents.json?'
 
-
-#If there were no errors, get the json
-#If a temporary error occured, sleep for 5 minutes and try again. Do this 50 times, and if it continues to fail, CallFailException
-#If a Permanent error occures, throw CallFailException
-#If the users ApiCount is zero, sleep for one hour to refresh the calls
 def api_call_manager(url):
+
+    """
+    :param url: the url that will be used to make the api call
+    :return: returns the json information of the documents
+
+    If there were no errors, get the json
+    If a temporary error occured, sleep for 5 minutes and try again. Do this 50 times, and if it continues to fail, CallFailException
+    If a Permanent error occures, throw CallFailException
+    If the users ApiCount is zero, sleep for one hour to refresh the calls
+    """
+
     pause = 0
     while (pause < 51):
         try:
