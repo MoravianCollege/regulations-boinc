3930ccc4cef0c326c4acd680cf48108e84fb100e
Charles Peeke <stcdp03@moravian.edu>
2018-06-22 15:20:57 -0400

    Add testing file for call processor and edited other test files

--- tests/test_api_managment.py
+++ tests/test_api_managment.py
@@ -4,7 +4,6 @@ from mock import *
 
 from api_call_managment import *
 
-key = os.environ['API_TOKEN_REGULATIONS_GOV']
 base_url = 'https://api.data.gov:443/regulations/v3/documents.json?'
 
 
@@ -12,6 +11,7 @@ base_url = 'https://api.data.gov:443/regulations/v3/documents.json?'
 def mock_req():
     with requests_mock.Mocker() as m:
         yield m
+
 
 def set_time():
     mock_time = Mock()
@@ -21,25 +21,25 @@ def set_time():
 
 def test_success(mock_req):
     mock_req.get(add_api_key(base_url), status_code=200, text='{}')
-    assert api_call_manager(add_api_key(base_url)) == {}
+    assert api_call_manager(base_url) == {}
 
 
 @patch('time.sleep', set_time())
-def test_retry_calls_Failure(mock_req):
+def test_retry_calls_failure(mock_req):
     mock_req.get(add_api_key(base_url), status_code=304)
     with pytest.raises(CallFailException):
-        api_call_manager(add_api_key(base_url))
+        api_call_manager(base_url)
 
 def test_callfailexception(mock_req):
     mock_req.get(add_api_key(base_url), status_code=403)
     with pytest.raises(CallFailException):
-        api_call_manager(add_api_key(base_url))
+        api_call_manager(base_url)
 
 
 @patch('time.sleep', set_time())
 def test_user_out_of_api_calls_sleeps(mock_req):
     mock_req.register_uri('GET', add_api_key(base_url), [{'text': 'resp1', 'status_code': 429},{'text': '{}', 'status_code': 200}])
-    assert api_call_manager(add_api_key(base_url)) == {}
+    assert api_call_manager(base_url) == {}
 
 
 
--- tests/test_call_file_processor.py
+++ tests/test_call_file_processor.py
@@ -2,32 +2,60 @@ from api_call_managment import *
 from work_accumulator import *
 from call_file_processor import *
 
-def test_call_file_processor():
+import pytest
+import requests_mock
+
+from api_call import add_api_key
+
+key = os.environ['API_TOKEN_REGULATIONS_GOV']
+base_url = 'https://api.data.gov:443/regulations/v3/documents.json?'
+
+
+'''
+def ignore_test_call_file_processor():
     assert call_file_processor()
+
+'''
+
+
+@pytest.fixture
+def mock_req():
+    with requests_mock.Mocker() as m:
+        yield m
+
 
 # PRE PROCESS
 
-def test_successful_call():
+'''
+def ignore_test_successful_call():
+    process_call()
     pass
+'''
 
 
-def test_call_fail_raises_exception():
-    pass
+def test_call_fail_raises_exception(mock_req):
+    mock_req.get(add_api_key(base_url), status_code=407, text='{}')
+    with pytest.raises(CallFailException):
+        process_call(base_url)
 
 
-def test_file_not_found():
+def ignore_test_file_not_found():
+    call_file_processor()
     pass
 
 # PROCESS
 
 
-def test_empty_json():
+def ignore_test_empty_json():
+    process_results()
     pass
 
 
-def test_bad_json_format():
+def ignore_test_bad_json_format():
+    process_results()
     pass
 
 
-def test_wa_integration():
+def ignore_test_wa_integration():
+    process_results()
     pass
