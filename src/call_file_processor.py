9be3e9f7c16097beaa0498a2163078f42b30322b
dstocker <chos3nmag3@gmail.com>
2018-06-25 13:59:06 -0400

    Added documentation and fixed exception raising

--- src/call_file_processor.py
+++ src/call_file_processor.py
@@ -5,10 +5,15 @@ import json
 
 
 def call_file_processor(filepath, dirpath):
-    try:
-        f = open(filepath, 'r')
-    except FileNotFoundError:
-        raise FileNotFoundError
+    """
+    Open the file found at file path
+    For each line in the file, make the appropriate api call
+    Process the results from the api call to generate work files
+    :param filepath: path of the file where the document api call will be read
+    :param dirpath: path of the directory where workfiles will be written
+    :return: returns True if the processor completed successfully
+    """
+    f = open(filepath, 'r')
     for line in f:
         result = process_call(line)
         process_results(result, dirpath)
@@ -16,14 +21,24 @@ def call_file_processor(filepath, dirpath):
 
 
 def process_call(url):
-    try:
-        result = api_call_manager(url)
-        return result
-    except CallFailException:
-        raise CallFailException
+    """
+    Uses the api call manager to get the information
+    :param url: the url that will be called using the api
+    :return: returns the result of the api call
+    """
+    result = api_call_manager(url)
+    return result
 
 
 def process_results(result, dirpath):
+    """
+    Loads the json from the results of the api call
+    Gets the list of documents from the json
+    Create a new work accumulator that organizes the documents returned from each api call
+    :param result: Result of the api call
+    :param dirpath: path of the directory where workfiles will be written
+    :return: returns True if the processing completed successfully
+    """
     try:
         docs_json = json.loads(result.text)
         doc_list = docs_json["documents"]
@@ -31,7 +46,6 @@ def process_results(result, dirpath):
         raise BadJsonException
     except TypeError:
         raise BadJsonException
-
     wa.work_accumulator(dirpath)
     for doc in doc_list:
         wa.add_doc(doc["documentId"], doc["attachmentCount"] + 1)
@@ -39,5 +53,6 @@ def process_results(result, dirpath):
     return True
 
 
+# Raised if the json is not correctly formatted or is empty
 class BadJsonException(Exception):
     print("NOTICE: The Json appears to be formatted incorrectly.")
