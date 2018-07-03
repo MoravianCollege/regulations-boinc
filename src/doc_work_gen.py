0ada899b9504f30bf5e1990eea7deefdbf58ff80
WBrandes <brandesw@moravian.edu>
2018-07-03 14:04:56 -0400

    Updated method calls to work with actual docker images

--- src/doc_work_gen.py
+++ src/doc_work_gen.py
@@ -7,4 +7,4 @@ work_server_key = os.environ["WORK_SERVER_KEY"]
 def build_job(work_list):
     random_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
     requests.post('http://10.76.100.145:5000/add_work', data=dict(job_id=random_id, job_units=work_list, key=work_server_key))
-    b2d.boinc2docker_create_work("csdev/boinc:docs", "python app.py " + random_id)
+    b2d.boinc2docker_create_work("csmoco1742/client:latest", "python document_processor.py " + random_id)
--- src/docs_work_gen.py
+++ src/docs_work_gen.py
@@ -60,7 +60,7 @@ def monolith():
             random_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
             #Sends the work to the work server and makes a job for the client
             requests.post('http://10.76.100.145:5000/add_work', data=dict(job_id=random_id, job_units=url_list, key=work_server_key))
-            b2d.boinc2docker_create_work("csdev/boinc:docs", "python app.py " + random_id)
+            b2d.boinc2docker_create_work("csmoco1742/client:latest", "python documents_processor.py m" + random_id)
 
         #Saving our new page index
         file_writable = open(key_file, "w")
